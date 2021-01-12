a71b0019829b8a25c23349463b5a99501dbb8ced
Hiroshi Miura <miurahr@linux.com>
2021-01-12 18:06:12 +0900

    GitHub actions (#1)
    * Disable CPack(DEB)
    * Github-Actions: run on ubuntu-20.04
    
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- Packaging.cmake
+++ Packaging.cmake
@@ -178,12 +178,12 @@ else()
         message(STATUS "   + RPM                                  NO ")
     endif()
 
-    list(APPEND CPACK_GENERATOR DEB)
-    message(STATUS "   + DEB                                  YES ")
+    # list(APPEND CPACK_GENERATOR DEB)
+    message(STATUS "   + DEB                                  NO ")
     set(CPACK_DEBIAN_PACKAGE_ARCHITECTURE "amd64")
     set(CPACK_DEBIAN_PACKAGE_CONTROL_STRICT_PERMISSION TRUE)
     set(CPACK_DEBIAN_PACKAGE_HOMEPAGE "${PROJECT_URL}")
-    set(CPACK_DEBIAN_PACKAGE_SHLIBDEPS ON)
+    set(CPACK_DEBIAN_PACKAGE_SHLIBDEPS OFF)  # ON When build with libraries only from debian packages
 
     if(LINUXDEPLOY_EXECUTABLE)
         message(STATUS "   + AppImage                             YES ")
